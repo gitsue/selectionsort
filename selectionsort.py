f13877834b8c6e457f5ac50767cbe2e8f06210c7
Sue Deng <spideng@gmail.com>
2016-09-06 10:36:41 -0700

    initial commit

---
+++ selectionsort.py
@@ -0,0 +1,9 @@
+import random
+
+#Create a sample list with 100 randomized numbers
+samplelist = random.sample(xrange(0,10001),5)
+
+print samplelist
+
+#Implement a selection sort algorithm that returns a new list without creating another list
+
